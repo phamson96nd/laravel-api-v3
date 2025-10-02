0583286da005b0ec8614a8ebbb2a6be645c6f66c
phamson96nd <phamson96nd@gmail.com>
2025-10-03 03:43:42 +0700

    change template test auto

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -17,8 +17,8 @@
     </head>
     <body class="antialiased">
 
-        <h1>Hello World</h1>    
-        
+        <h1>v4</h1>    
+
         <div class="relative sm:flex sm:justify-center sm:items-center min-h-screen bg-dots-darker bg-center bg-gray-100 dark:bg-dots-lighter dark:bg-gray-900 selection:bg-red-500 selection:text-white">
             @if (Route::has('login'))
                 <div class="sm:fixed sm:top-0 sm:right-0 p-6 text-right z-10">
