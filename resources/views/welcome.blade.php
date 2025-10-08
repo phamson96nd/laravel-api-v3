e417d302b50233f7ddcbdc500b0bbb91e6e472af
phamson96nd <phamson96nd@gmail.com>
2025-10-08 10:21:44 +0700

    a

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -17,7 +17,7 @@
     </head>
     <body class="antialiased">
 
-        <h1>v19</h1>    
+        <h1>v20</h1>    
 
         <div class="relative sm:flex sm:justify-center sm:items-center min-h-screen bg-dots-darker bg-center bg-gray-100 dark:bg-dots-lighter dark:bg-gray-900 selection:bg-red-500 selection:text-white">
             @if (Route::has('login'))
